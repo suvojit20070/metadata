d0eef7617e194957659db9ca5dab6d23a1587c39
suvojit20070 <suvo27804@gmail.com>
2026-08-04 09:18:25 +0530

    Fix async fetch call in metadata route

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ res.json({
 });
 
 app.get("/metadata", (req, res) => {
-  const response = await fetch("https://cdn.jsdelivr.net/gh/suvojit20070/metadata@main/index.html");
-  const html = response.text();
+  const response = fetch("https://cdn.jsdelivr.net/gh/suvojit20070/metadata@main/index.html");
+  const html = await response.text();
   res.setHeader("Content-Type", "text/html");
   res.send(html);
 });
