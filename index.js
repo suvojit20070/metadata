b3ba5e3e15c6dde1d641b70f9934ecece70a2b4b
suvojit20070 <suvo27804@gmail.com>
2026-08-04 08:56:09 +0530

    Update Lottie animation path to CDN

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ Images • Videos • Audio • Documents • Archives • Stickers • Any publ
     renderer: "svg",
     loop: true,
     autoplay: true,
-    path: "https://raw.githubusercontent.com/suvojit20070/metadata/main/tg-logo.json" // Local file path
+    path: "https://cdn.jsdelivr.net/gh/suvojit20070/metadata@main/tg-logo.json"
   });
 </script>
 
